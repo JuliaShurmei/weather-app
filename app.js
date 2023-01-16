901cbc65fb1296f87c3eb94a9c81c231f31b78bb
JuliaShurmei <ivanovaulia428@gmail.com>
2023-01-16 12:00:00 +0300

    Fixed functionality

--- app.js
+++ app.js
@@ -8,3 +8,31 @@ let weather__forecast = document.querySelector('.weather__forecast');
 let weather__temperature = document.querySelector(".weather__temperature");
 let weather__icon = document.querySelector(".weather__icon");
 let weather__minmax = document.querySelector(".weather__minmax")
+let weather__realfeel = document.querySelector('.weather__realfeel');
+let weather__humidity = document.querySelector('.weather__humidity');
+let weather__wind = document.querySelector('.weather__wind');
+let weather__pressure = document.querySelector('.weather__pressure');
+
+// search
+document.querySelector(".weather__search").addEventListener('submit', e => {
+    let search = document.querySelector(".weather__searchform");
+    // prevent default action
+    e.preventDefault();
+    // change current city
+    currCity = search.value;
+    // get weather forecast 
+    getWeather();
+    // clear form
+    search.value = ""
+})
+
+// units
+document.querySelector(".weather_unit_celsius").addEventListener('click', () => {
+    if(units !== "metric"){
+        // change to metric
+        units = "metric"
+        // get weather forecast 
+        getWeather()
+    }
+})
+
