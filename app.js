e3abecf80a94cff841b9ea786ec2a50fdaaaeee9
JuliaShurmei <ivanovaulia428@gmail.com>
2023-01-17 12:00:00 +0300

    Fixed functionality

--- app.js
+++ app.js
@@ -8,6 +8,3 @@ let weather__forecast = document.querySelector('.weather__forecast');
 let weather__temperature = document.querySelector(".weather__temperature");
 let weather__icon = document.querySelector(".weather__icon");
 let weather__minmax = document.querySelector(".weather__minmax")
-let weather__realfeel = document.querySelector('.weather__realfeel');
-let weather__humidity = document.querySelector('.weather__humidity');
-let weather__wind = document.querySelector('.weather__wind');
