81e6749438e6d166d53e2c5042e54e20e907cd09
JuliaShurmei <ivanovaulia428@gmail.com>
2023-01-18 12:00:00 +0300

    Fixed functionality

--- app.js
+++ app.js
@@ -14,25 +14,6 @@ let weather__wind = document.querySelector('.weather__wind');
 let weather__pressure = document.querySelector('.weather__pressure');
 
 
-function convertTimeStamp(timestamp, timezone){
-     const convertTimezone = timezone / 3600; // convert seconds to hours 
-
-    const date = new Date(timestamp * 1000);
-    
-    const options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        timeZone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(convertTimezone)}`,
-        hour12: true,
-    }
-    return date.toLocaleString("en-US", options)
-   
-}
-
  
 
 // convert country code to name
